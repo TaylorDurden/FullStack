92069de1c70fca7f25bc3a979c8fe382612f4558
178453765@qq.com <178453765@qq.com>
2016-12-10 16:31:21 +0800

    adding practice folder to exam new tools or techniques

---
+++ practice/server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/12/10.
+ */
+var http = require("http");
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Hello World!");
+    response.end();
+}).listen(8080);
+console.log("Server running at http://localhost:8080/");
