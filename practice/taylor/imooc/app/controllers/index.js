5dd561fd393c4c9aeae473c4aa5eb71cfaf3c3c9
Taylor <258266864@qq.com>
2017-01-14 19:49:36 +0800

    next to next chapter enhance admin function

--- practice/taylor/imooc/app/controllers/index.js
+++ practice/taylor/imooc/app/controllers/index.js
@@ -10,7 +10,10 @@ exports.index = function(req, res) {
 
     Category
         .find({})
-        .populate({path: 'movies', options: {limit: 5}})
+        .populate({
+            path: 'movies',
+            options: { limit: 6 }
+        })
         .exec(function(err, categories) {
             if (err) console.log(err);
 
@@ -19,4 +22,56 @@ exports.index = function(req, res) {
                 categories: categories
             });
         });
+};
+
+// search page
+exports.search = function(req, res) {
+    var categoryId = req.query.categoryId;
+    var page = parseInt(req.query.p, 10) || 0;
+    var q = req.query.q;
+    var count = 2;
+    var index = page * count;
+
+    if (categoryId){
+        Category
+            .find({ _id: categoryId })
+            .populate({
+                path: 'movies',
+                select: 'title poster'
+            })
+            .exec(function(err, categories) {
+                if (err) console.log(err);
+
+                var category = categories[0] || {};
+                var movies = category.movies || [];
+                var results = movies.slice(index, index+count);
+
+                res.render('results', {
+                    title: 'imooc 结果列表页面',
+                    keyword: category.name,
+                    currentPage: (page + 1),
+                    query: 'category='+categoryId,
+                    totalPage: Math.ceil(movies.length / count),
+                    results: results
+                });
+            });
+    }
+    else {
+        Movie
+            .find({title: new RegExp(q+ '.*', 'i')})
+            .exec(function(err, movies) {
+                if (err) console.log(err);
+
+                var results = movies.slice(index, index+count);
+
+                res.render('results', {
+                    title: 'imooc 结果列表页面',
+                    keyword: q,
+                    currentPage: (page + 1),
+                    query: 'q='+q,
+                    totalPage: Math.ceil(movies.length / count),
+                    movies: results
+                });
+            })
+    }
 };
